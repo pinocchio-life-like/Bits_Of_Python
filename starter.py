4208c4ed13ede9d89b208a57955728c20d4187dc
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-11 08:47:04 +0300

    first commit

--- starter.py
+++ starter.py
@@ -5,15 +5,11 @@ r = sr.Recognizer()
 
 # use the default microphone as the audio source
 with sr.Microphone() as source:
-    print("Speak now...")
-    # listen for audio and convert it to text
-    audio = r.listen(source)
+    print("Speak something...")
+    # listen for audio and store it in audio_data variable
+    audio_data = r.record(source, duration=5)
+    print("Recognizing...")
 
-try:
     # recognize speech using Google Speech Recognition
-    text = r.recognize_google(audio, language='en-US')
-    print("You said: ", text)
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    text = r.recognize_google(audio_data)
+    print(f"You said: {text}")
