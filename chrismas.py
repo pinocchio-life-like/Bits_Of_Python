1f4d87209d295c6d869aabca549cdaf774eaba7d
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-13 14:20:33 +0300

    first commit

--- chrismas.py
+++ chrismas.py
@@ -1,6 +1,20 @@
-for i in range(10):
-    spaces = " "*(10-i)
-    hashes = "*"*(2*i-1)
-    print(spaces + hashes)
-for i in range(3):
-    print(f'{" "*7}{"#"*5}')
+#Exercise Build christmass tree
+# for i in range(10):
+#     spaces = " "*(10-i)
+#     hashes = "*"*(2*i-1)
+#     print(spaces + hashes)
+# for i in range(3):
+#     print(f'{" "*7}{"#"*5}')
+
+
+#Exercise: Check for duplicates in list:
+
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+for i in some_list:
+    if some_list.count(i) > 1:
+        duplicates.append(i)
+dup = set(duplicates)
+
+print(dup)
