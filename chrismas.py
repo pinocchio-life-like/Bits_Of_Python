7febaba5880125f0447a5fe739ca602f96dd4332
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-13 11:49:00 +0300

    first commit

--- chrismas.py
+++ chrismas.py
@@ -1,6 +1,4 @@
-for i in range(1, 10):
-    spaces = " " * (10 - i)
-    hashes = "#" * (2 * i - 1)
+for i in range(10):
+    spaces = " "*(10-i)
+    hashes = "*"*(2*i-1)
     print(spaces + hashes)
-for i in range(3):
-    print(f'{" "*7}{"#"*4}')
