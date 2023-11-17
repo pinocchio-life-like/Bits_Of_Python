9b8c860020bca59b80f2e464f289883d2644606d
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-17 17:05:57 +0300

    Classes In python

--- error_handling.py
+++ error_handling.py
@@ -2,5 +2,13 @@ def sum(num1, num2):
     while True:
         try:
             return num1+num2
-        except:
-            print("You have made a mistake!")
+        except  TypeError as err:
+            print("You have made a mistake!", err)
+        else:
+            print("I run if no error or no break")
+            print("I also can not come right after try")
+        finally:
+            print("I run no matter what")
+            break
+        
+sum(1,2)
