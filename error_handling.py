40c1b796509bff18f3ae94357e4691ff92fab73a
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-17 16:48:31 +0300

    Classes In python

--- error_handling.py
+++ error_handling.py
@@ -1,5 +1,6 @@
 def sum(num1, num2):
-    try:
-        return num1+num2
-    except:
-        print("Something is wrong!")
+    while True:
+        try:
+            return num1+num2
+        except:
+            print("You have made a mistake!")
