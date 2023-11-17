50828d87ca8643c56d6d11036f1b1286b3b87ac5
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-17 14:02:45 +0300

    Classes In python

--- decorators.py
+++ decorators.py
@@ -1,14 +1,12 @@
 def decorator(func):
     def wrapper_func(*args, **kwargs):
-        print("##########")
+        print("********")
         func(*args, **kwargs)
-        print("##########")
+        print("********")
     return wrapper_func
 
 @decorator
 def hello_func(*args, **kwargs):
     print(*args)
     
-
 hello_func("Helloooo")
- 
