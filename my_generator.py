19167fbe41deefcba71926447de194de46cbac74
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-18 15:32:27 +0300

    Classes In python

---
+++ my_generator.py
@@ -0,0 +1,22 @@
+class MyGenerator:
+    def __init__(self, init, last):
+        self.init = init
+        self.curr = init
+        self.last = last
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.curr < self.last:
+            num = self.curr
+            self.curr +=1
+            return num
+        else:
+            raise StopIteration
+        
+gen = MyGenerator(2,5)
+
+for i in gen:
+    print(i)
+         
