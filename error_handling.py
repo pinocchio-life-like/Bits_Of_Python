89a54bd8dc94e88535d6a8d45502fe67c5a1aca0
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-18 11:46:49 +0300

    Classes In python

--- error_handling.py
+++ error_handling.py
@@ -2,6 +2,10 @@ def sum(num1, num2):
     while True:
         try:
             return num1+num2
+        # Also you can do raise your won error as
+        #raise ValueError("The message to print")
+        #the above is used when you want to raise your won
+        #custom error.
         except  TypeError as err:
             print("You have made a mistake!", err)
         else:
