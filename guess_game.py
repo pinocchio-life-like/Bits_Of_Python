eef70b077e45d7a5849f106375cd0247a661a06f
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-22 16:20:09 +0300

    pyhton module exercise

--- guess_game.py
+++ guess_game.py
@@ -1,21 +1,21 @@
-from random import choice
+from random import randint
 
 print("This is a guess game, pick a number between [1, 10]")
-num = choice([1,2,3,4,5,6,7,8,9,10])
+num = randint(1,10)
 
 trial = 0
 
 while True:
     if trial > 0:
         guess = input("Pick again: ")
-        if num == int(guess, base=10):
+        if num == guess:
             print("You are a genius")
             break
         else:
             print("You are wrong guess again")
     else:
         guess = input("Welcome, pick from [1,10]: ")
-        if num == int(guess, base=10):
+        if num == guess:
             print("You are a genius")
             break
         else:
