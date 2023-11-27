0db6dd306e4b3fa53b5ebddb896daed6d8640316
pinocchio-life-like <icbr19fl@gmail.com>
2023-11-27 09:56:02 +0300

    leet code

--- palindrome.py
+++ palindrome.py
@@ -5,9 +5,5 @@ def isPalindrome(x):
     elif x == 0:
         return True
     else:
-        reverse = 0
-        temp = x
-        while temp > 0:
-            reverse = (reverse * 10) + (temp % 10)
-            temp = temp // 10
-        return reverse == x
+        return str(x) == str(x)[::-1]
+
