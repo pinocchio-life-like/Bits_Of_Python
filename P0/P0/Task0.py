d6f328994777b006cc608fa8601a7e8eb8231bb4
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-27 09:48:47 +0300

    leet problems

--- P0/P0/Task0.py
+++ P0/P0/Task0.py
@@ -19,4 +19,8 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
+incoming_number = texts[0][0]
+answering_number = texts[0][1]
+at_time = texts[0][2]
 
+print(f"First record of texts, {incoming_number} texts {answering_number} at time {at_time}")
