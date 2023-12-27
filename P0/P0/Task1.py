11039aa203884bdd9493c50fdb08c3efc419d022
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-27 10:07:03 +0300

    leet problems

--- P0/P0/Task1.py
+++ P0/P0/Task1.py
@@ -18,6 +18,6 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
-numbers = set(texts[:][0]).union(set(calls[:][0]))
-
+numbers = set(texts[0:-1])
 print(f"There are {len(numbers)} different telephone numbers in the records.")
+print(numbers)
