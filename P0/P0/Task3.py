8db7af7420a702092a4f8c68c1c1c1aaf5de43ae
pinocchio-life-like <icbr19fl@gmail.com>
2023-12-28 08:06:55 +0300

    leet problems

--- P0/P0/Task3.py
+++ P0/P0/Task3.py
@@ -28,18 +28,22 @@ in Bangalore.
  - Telemarketers' numbers have no parentheses or space, but they start
    with the area code 140.
 
+
 Print the answer as part of a message:
 "The numbers called by people in Bangalore have codes:"
  <list of codes>
 The list of codes should be print out one per line in lexicographic order with no duplicates.
+
 
 Part B: What percentage of calls from fixed lines in Bangalore are made
 to fixed lines also in Bangalore? In other words, of all the calls made
 from a number starting with "(080)", what percentage of these calls
 were made to a number also starting with "(080)"?
 
+
 Print the answer as a part of a message::
 "<percentage> percent of calls from fixed lines in Bangalore are calls
 to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
+
